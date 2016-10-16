91a49b5601c8419c8761b8809fcc03365c199530
Khoa Bui <buihdk@gmail.com>
2016-10-16 17:42:32 +0700

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :food_items
+  root 'home#index'
+  get 'menu' => 'home#menu'
+  get 'contact_us' => 'home#contact_us'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
